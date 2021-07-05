993552ea0ad5dcde65179342fd70cc09fe87b976
DESKTOP-2O3NDID\user <lennykmutua@gmail.com>
2021-07-05 13:27:34 +0300

    Add new file

--- power.py
+++ power.py
@@ -1,13 +1,13 @@
-import os
 import psutil
-import pyautogui
-from playsound import playsound
+from pyautogui import alert
 
 battery = psutil.sensors_battery()
 plugged = battery.power_plugged
 percent = battery.percent
 
 if plugged and percent >= 100:
-    mp3 = f'{os.getcwd()}\\notification_sound.mp3'
-    playsound(mp3)
-    pyautogui.alert('Battery full, unplug charger!')
+    alert(
+        text='Your Battery is Fully Charged, Please Unplug Your Charger!',
+        title='BATTERY FULL NOTIFICATION',
+        button='OK'
+    )
